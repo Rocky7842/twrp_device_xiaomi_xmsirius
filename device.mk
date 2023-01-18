ed8cd8eef3f59dd59c367e14f2c9a158fbbe760e
me-cafebabe <me_cafebabe@hotmail.com>
2023-01-18 17:59:05 +0800

    xmsirius: Support separated vendor tree

--- device.mk
+++ device.mk
@@ -13,6 +13,12 @@ MI710_KEYMASTER_VERSION := 3.0
 # Shipping API Level
 PRODUCT_SHIPPING_API_LEVEL := 27
 
+# Vendor blobs
+ifneq ($(wildcard vendor/xiaomi/xmsirius/proprietary/),)
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/xmsirius/proprietary/vendor/,$(TARGET_COPY_OUT_RECOVERY)/root/system/)
+endif
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
