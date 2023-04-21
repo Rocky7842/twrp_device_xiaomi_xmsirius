e733e69b87ce64ec2cf659f1ee96853dac412ce0
Rocky7842 <49489159+Rocky7842@users.noreply.github.com>
2023-04-21 22:07:14 +0800

    xmsirius: Fix device props

--- omni_xmsirius.mk
+++ omni_xmsirius.mk
@@ -34,8 +34,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
-    ro.product.device \
-    ro.product.name \
-    ro.build.product \
     ro.bootimage.build.date.utc \
     ro.build.date.utc
