5154ffc157083469b7f045b8a2fac7c2e71d1a12
Rocky7842 <49489159+Rocky7842@users.noreply.github.com>
2023-10-04 21:43:12 +0800

    xmsirius: Inherit from common tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from common sdm710-common
+include device/xiaomi/sdm710-common/BoardConfigCommon.mk
+
 DEVICE_PATH := device/xiaomi/xmsirius
 
 # Recovery
--- device.mk
+++ device.mk
@@ -4,6 +4,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from sdm710-common
+$(call inherit-product, device/xiaomi/sdm710-common/sdm710.mk)
+
+# Crypto
+MI710_KEYMASTER_VERSION := 3.0
+
 # Shipping API Level
 PRODUCT_SHIPPING_API_LEVEL := 27
 
